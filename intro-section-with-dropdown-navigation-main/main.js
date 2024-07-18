01008ffb76b925aedd015d7c4d242d9c5016f973
lumalnis <luciamalnis@gmail.com>
2024-07-18 15:02:07 -0300

    Challenge Finished

--- intro-section-with-dropdown-navigation-main/main.js
+++ intro-section-with-dropdown-navigation-main/main.js
@@ -1,6 +1,12 @@
 const hamburguerMenu = document.querySelector('#hamburguer-menu');
 const navbar = document.getElementById('navbar');
 const closeBtn = document.querySelector('#close-btn');
+const featuresBtn = document.querySelector('#features-btn');
+const featuresUl = document.querySelector('#features-ul');
+const companyBtn = document.querySelector('#company-btn');
+const companyUl = document.querySelector('#company-ul');
+const featuresBtnArrow = document.querySelector('#features-btn-arrow');
+const companyBtnArrow = document.querySelector('#company-btn-arrow');
 
 hamburguerMenu.addEventListener('click', () => {
     navbar.style.display = 'block'
@@ -8,18 +14,36 @@ hamburguerMenu.addEventListener('click', () => {
 })
 
 closeBtn.addEventListener('click', () => {
-    navbar.style.display = 'none'
+
+    const screenSize = window.innerWidth;
+
+    if (screenSize >= 768) {
+        navbar.style.display = 'block'
+    } else {
+        navbar.style.display = 'none' /*esto lo cierra y si cambio de vista queda cerrado*/
+    } 
+})
+
+featuresBtn.addEventListener('click', () => {
+    if (featuresUl.style.display === 'block') {
+        featuresUl.style.display = 'none'
+    } else { featuresUl.style.display = 'block' }
+
+    /// modificar los estilos de las flechas
+    featuresBtnArrow.style.transition = 'transform 0.3s ease'
+    featuresBtnArrow.style.transform = (featuresBtnArrow.style.transform === 'rotate(180deg)') ? 'rotate(0deg)' : 'rotate(180deg)'
+
+})
+
+/*con if ternario*/
+companyBtn.addEventListener('click', () => {
+    companyUl.style.display = (companyUl.style.display === 'block') ? 'none' : 'block'
+
+      /// modificar los estilos de las flechas
+      companyBtnArrow.style.transition = 'transform 0.3s ease'
+      companyBtnArrow.style.transform = (companyBtnArrow.style.transform === 'rotate(180deg)') ? 'rotate(0deg)' : 'rotate(180deg)'
 })
 
 
-const submenuBtn = document.querySelector('.submenu-btn');
-const featuresUl = document.querySelector('.features-ul'); 
-const companyUl  = document.getElementsByClassName('company-ul');
 
-submenuBtn.addEventListener('click', ()=> {
-    featuresUl.style.display = 'block'
-})
 
-submenuBtn.addEventListener('click', ()=> {
-    companyUl.style.display = 'block'
-})
